fb2f229d016751a33e61b6e5200688f3d3e2617a
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-06 19:29:02 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -23,4 +23,11 @@ class SchoolClassController < ApplicationController
     @school_class.update(post_params(:title, :room_number))
     redirect_to school_class_path(@school_class)
   end
+
+
+  private
+
+  def post_params(*args)
+    params.require(:school_class).permit(*args)
+  end 
 end
