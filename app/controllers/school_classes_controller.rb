84388151a877e57fe8c7827b64551f850142c5ca
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-06 19:26:41 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,9 +1,13 @@
 class SchoolClassController < ApplicationController
 
   def new
+    @school_class = SchoolClass.new
   end
 
   def create
+    @school_class = SchoolClass.new(post_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
   end
 
   def show
@@ -13,5 +17,5 @@ class SchoolClassController < ApplicationController
   end
 
   def update
-  end 
+  end
 end
